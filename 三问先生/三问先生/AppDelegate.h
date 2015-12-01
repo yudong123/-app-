0f029e72f7e2c27fca4f9f1cc2fb5872ca2c7ac6
yudong123 <15208380120@163.com>
2015-12-01 15:29:05 +0800

    初始化项目

---
+++ 三问先生/三问先生/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  三问先生
+//
+//  Created by ma on 15/12/1.
+//  Copyright © 2015年 ma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 三问先生/三问先生/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  三问先生
+//
+//  Created by ma on 15/12/1.
+//  Copyright © 2015年 ma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
